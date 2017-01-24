b2777cea5b7a970e3836e0c76c255f8d3af8e9e8
lyle <lyle>
2017-01-24 13:52:04 +0800

    update

--- src/com/lyle/p3/单例模式/App.java
+++ src/com/lyle/p3/单例模式/App.java
@@ -3,9 +3,11 @@ package com.lyle.p3.单例模式;
 public class App {
 
 	public static void main(String[] args) {
-		Singleton one = Singleton.getInstance();
-		Singleton two = Singleton.getInstance();
-		System.out.println(one);
-		System.out.println(two);
+		Singleton inst1 = Singleton.getInstance();
+		Singleton inst2 = Singleton.getInstance();
+		Singleton inst3 = Singleton.getInstance();
+		System.out.println(inst1);
+		System.out.println(inst2);
+		System.out.print(inst3);
 	}
 }
--- src/com/lyle/p3/单例模式/Singleton.java
+++ src/com/lyle/p3/单例模式/Singleton.java
@@ -1,17 +1,28 @@
 package com.lyle.p3.单例模式;
 
+/**
+ * @ClassName: Singleton
+ * @Description: 饿汉模式
+ * @author: Lyle
+ * @date: 2017年1月24日 下午1:50:15
+ */
 public class Singleton {
 
-	private static Singleton single;
-	
+	/*
+	 * 私有静态实例
+	 */
+	private static Singleton single = new Singleton();
+
+	/*
+	 * 私有构造器
+	 */
 	private Singleton() {
-		
 	}
-	
-	public static Singleton getInstance(){
-		if(single == null){
-			single = new Singleton();
-		}
+
+	/*
+	 * 静态方法，创建实例
+	 */
+	public static Singleton getInstance() {
 		return single;
 	}
 }
